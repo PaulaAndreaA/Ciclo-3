8ddd1ce5c415d7df39f56dabe1e3e0490cab8623
Paula Alvarez <parc56725@gmail.com>
2022-09-10 18:42:14 -0500

    se crearon los controladores del Sprint3

---
+++ src/main/java/com/mycompany/ciclo3/Controller/EmpresaController.java
@@ -0,0 +1,53 @@
+package com.mycompany.ciclo3.Controller;
+
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.mycompany.ciclo3.Empresa;
+
+@RestController
+public class EmpresaController {
+
+    @GetMapping(value="/enterprises")
+    public ResponseEntity<List<Empresa>> consultarEmpresas(){
+
+        return new ResponseEntity<>(HttpStatus.OK);
+        
+    }
+
+    @PostMapping(value="/enterprises")
+    public ResponseEntity<Empresa> crearEmpresas(){
+
+        return new ResponseEntity<>(HttpStatus.OK);
+        
+    }
+
+    @GetMapping(value="/enterprises/{empresaId}")
+    public ResponseEntity<Empresa> consultarEmpresaEntity(){
+
+        return new ResponseEntity<>(HttpStatus.OK);
+        
+    }
+
+    @PatchMapping(value="/enterprises/{empresaId}")
+    public ResponseEntity<Empresa> actualizarEmpresa(){
+
+        return new ResponseEntity<>(HttpStatus.OK);
+        
+    }
+
+    @DeleteMapping(value="/enterprises/{empresaId}")
+    public ResponseEntity<String> eliminarEmpresa(){
+
+        return new ResponseEntity<>("eliminar empresa", HttpStatus.OK);         
+    }
+
+
+}
